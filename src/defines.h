cdc59315cc2ed047106b30e12b310c558b912a54
Fabrice <fabrice@schaub-dev.xyz>
2026-02-15 21:55:36 +0100

    working on defines

---
+++ src/defines.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+#if UINTPTR_MAX == UINT64_MAX
+typedef u64 usize;
+typedef i64 isize;
+#elif UINTPTR_MAX == UINT32_MAX
+typedef u32 usize;
+typedef i32 isize;
+#else
+#error "Unsupported pointer width"
+#endif
+
+struct span_s {
+  u8 *ptr;
+  usize len;
+};
+
+#define span_init(ptr, len) ((struct span_s){.ptr = (ptr), .len = (len)})
+#define span_ptr(span) ((span).ptr)
+#define span_len(span) ((span).len)
